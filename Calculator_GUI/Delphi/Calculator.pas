992dd59a32c82578beb48ee349c8dd7b5a6026d2
Emad Bin Abid <emad.bin.abid@gmail.com>
2023-02-17 15:53:11 +0500

    Add files via upload

---
+++ Calculator_GUI/Delphi/Calculator.pas
@@ -0,0 +1,163 @@
+unit Calculator;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.StdCtrls,
+  FMX.Controls.Presentation;
+
+type
+  TForm2 = class(TForm)
+    Del: TButton;
+    C: TButton;
+    Percent: TButton;
+    Plus: TButton;
+    Seven: TButton;
+    Eight: TButton;
+    Nine: TButton;
+    Minus: TButton;
+    Four: TButton;
+    Five: TButton;
+    Six: TButton;
+    Multiply: TButton;
+    One: TButton;
+    Two: TButton;
+    Three: TButton;
+    Zero: TButton;
+    Dot: TButton;
+    Divide: TButton;
+    Equals: TButton;
+    Results: TLabel;
+    Calc: TLabel;
+    procedure DelClick(Sender: TObject);
+    procedure ResultsClick(Sender: TObject);
+    procedure CClick(Sender: TObject);
+    procedure PercentClick(Sender: TObject);
+    procedure PlusClick(Sender: TObject);
+    procedure SevenClick(Sender: TObject);
+    procedure EightClick(Sender: TObject);
+    procedure NineClick(Sender: TObject);
+    procedure MinusClick(Sender: TObject);
+    procedure FourClick(Sender: TObject);
+    procedure FiveClick(Sender: TObject);
+    procedure SixClick(Sender: TObject);
+    procedure MultiplyClick(Sender: TObject);
+    procedure OneClick(Sender: TObject);
+    procedure TwoClick(Sender: TObject);
+    procedure ThreeClick(Sender: TObject);
+    procedure DivideClick(Sender: TObject);
+    procedure ZeroClick(Sender: TObject);
+    procedure DotClick(Sender: TObject);
+    procedure EqualsClick(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form2: TForm2;
+
+implementation
+
+{$R *.fmx}
+
+procedure TForm2.DelClick(Sender: TObject);
+begin
+     //
+end;procedure TForm2.ResultsClick(Sender: TObject);
+begin
+     //
+end;
+procedure TForm2.CClick(Sender: TObject);
+begin
+     //
+end;
+
+procedure TForm2.DivideClick(Sender: TObject);
+begin
+     //
+end;
+
+procedure TForm2.DotClick(Sender: TObject);
+begin
+     //
+end;
+
+procedure TForm2.EightClick(Sender: TObject);
+begin
+     //
+end;
+
+procedure TForm2.EqualsClick(Sender: TObject);
+begin
+     //
+end;
+
+procedure TForm2.FiveClick(Sender: TObject);
+begin
+     //
+end;
+
+procedure TForm2.FourClick(Sender: TObject);
+begin
+     //
+end;
+
+procedure TForm2.MinusClick(Sender: TObject);
+begin
+     //
+end;
+
+procedure TForm2.MultiplyClick(Sender: TObject);
+begin
+     //
+end;
+
+procedure TForm2.NineClick(Sender: TObject);
+begin
+     //
+end;
+
+procedure TForm2.OneClick(Sender: TObject);
+begin
+     //
+end;
+
+procedure TForm2.PercentClick(Sender: TObject);
+begin
+     //
+end;
+
+procedure TForm2.PlusClick(Sender: TObject);
+begin
+     //
+end;
+
+procedure TForm2.SevenClick(Sender: TObject);
+begin
+     //
+end;
+
+procedure TForm2.SixClick(Sender: TObject);
+begin
+     //
+end;
+
+procedure TForm2.ThreeClick(Sender: TObject);
+begin
+     //
+end;
+
+procedure TForm2.TwoClick(Sender: TObject);
+begin
+     //
+end;
+
+procedure TForm2.ZeroClick(Sender: TObject);
+begin
+     //
+end;
+
+end.
