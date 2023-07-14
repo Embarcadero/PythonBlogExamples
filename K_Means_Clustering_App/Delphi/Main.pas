6812cc9a695641fe1f57b438382fa73c47793b85
Emad Bin Abid <emad.bin.abid@gmail.com>
2023-07-14 12:02:46 +0500

    Add Delphi files to K means clustering app.

--- K_Means_Clustering_App/Delphi/Main.pas
+++ K_Means_Clustering_App/Delphi/Main.pas
@@ -1,1 +1,46 @@
+unit Main;
 
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Objects,
+  FMX.StdCtrls, FMX.Edit, FMX.Controls.Presentation;
+
+type
+  TMainForm = class(TForm)
+    Title: TLabel;
+    KValue: TEdit;
+    KLabel: TLabel;
+    ApplyButton: TButton;
+    UploadButton: TButton;
+    OpenDialog1: TOpenDialog;
+    BeforeImage: TImage;
+    AfterImage: TImage;
+    StyleBook1: TStyleBook;
+    procedure UploadButtonClick(Sender: TObject);
+    procedure ApplyButtonClick(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  MainForm: TMainForm;
+
+implementation
+
+{$R *.fmx}
+
+procedure TMainForm.ApplyButtonClick(Sender: TObject);
+begin
+//
+end;
+
+procedure TMainForm.UploadButtonClick(Sender: TObject);
+begin
+//
+end;
+
+end.
