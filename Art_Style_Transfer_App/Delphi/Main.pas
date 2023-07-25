30c3d2887986ea377b7f7c5f33fd76b19c58d370
Emad Bin Abid <emad.bin.abid@gmail.com>
2023-07-25 14:21:57 +0500

    Add Delphi projects files for Art Style Transfer App.

--- Art_Style_Transfer_App/Delphi/Main.pas
+++ Art_Style_Transfer_App/Delphi/Main.pas
@@ -1,1 +1,55 @@
+unit Main;
 
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Objects,
+  FMX.StdCtrls, FMX.Controls.Presentation;
+
+type
+  TMainForm = class(TForm)
+    Title: TLabel;
+    StyleImageLabel: TLabel;
+    ContentImageLabel: TLabel;
+    UploadContentButton: TButton;
+    UploadStyleButton: TButton;
+    ApplyButton: TButton;
+    StyleImage: TImage;
+    ContentImage: TImage;
+    FinalImage: TImage;
+    OpenDialog1: TOpenDialog;
+    OpenDialog2: TOpenDialog;
+    StyleBook1: TStyleBook;
+    procedure UploadStyleButtonClick(Sender: TObject);
+    procedure UploadContentButtonClick(Sender: TObject);
+    procedure ApplyButtonClick(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  MainForm: TMainForm;
+
+implementation
+
+{$R *.fmx}
+
+procedure TMainForm.ApplyButtonClick(Sender: TObject);
+begin
+//
+end;
+
+procedure TMainForm.UploadContentButtonClick(Sender: TObject);
+begin
+//
+end;
+
+procedure TMainForm.UploadStyleButtonClick(Sender: TObject);
+begin
+//
+end;
+
+end.
